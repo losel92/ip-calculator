c27d4a6eccf01ccc191d9b8edc69f7351b1e7970
Sherab Losel Matos <loselmatos@gmail.com>
2019-06-07 13:12:53 +0200

    bugfixes and subnet mask calculation

--- FinalProjectGF2/Program.cs
+++ FinalProjectGF2/Program.cs
@@ -14,7 +14,9 @@ namespace FinalProjectGF2
             int[] intSec = new int[4];
             string[] inputExploded;
             string ipAddr = "";
+            string ipAddrBin = "";
             string subMaskStr = "";
+            string subMaskBin = "";
 
             //Introduction
             Console.WriteLine("Welcome to the Ip Calculator 2000");
@@ -22,9 +24,13 @@ namespace FinalProjectGF2
             Console.WriteLine("Please enter the Ip address either as dotted decimal or prefix");
             Console.WriteLine("Use these two templates as a reference:");
             Console.WriteLine("192.128.1.0/255.255.255.0  ||  192.128.1.0/24\n\n");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
             Start();
 
             void Start() {
+                Console.Clear();
+
                 Console.Write("Insert IP Address: ");
                 //Gets the Ip address to be manipulated
                 string userInput = Console.ReadLine();
@@ -36,6 +42,10 @@ namespace FinalProjectGF2
                 Console.WriteLine(ipAddr);
                 Console.SetCursorPosition(Console.CursorLeft + 10, Console.CursorTop + 3);
                 Console.WriteLine(subMaskStr);
+                Console.SetCursorPosition(Console.CursorLeft + 10, Console.CursorTop + 3);
+                Console.WriteLine(ipAddrBin);
+                Console.SetCursorPosition(Console.CursorLeft + 10, Console.CursorTop + 3);
+                Console.WriteLine(subMaskBin);
 
 
                 Console.ReadKey();
@@ -66,9 +76,11 @@ namespace FinalProjectGF2
                             //Gets each individual Ip field
                             string[] ipExploded = ipAddr.Split('.');
                             int[] ipExplodedInt = new int[4];
+                            string[] ipBin = new string[4];
 
                             string[] subnetExploded = new string[4];
                             int[] subnetInt = new int[4];
+                            string[] subnetBin = new string[4];
 
                             //This means the user is entering a prefix for the subnet
                             if (subMaskRaw.Length < 3 && subMaskRaw.Length > 0 && Convert.ToInt32(subMaskRaw) > 1 && Convert.ToInt32(subMaskRaw) < 31)
@@ -91,14 +103,25 @@ namespace FinalProjectGF2
                                         i = 0;
                                     }
 
-                                    //Translates from number of bits to decimal and assigns it to the section
+                                    //Translates from prefix to decimal and assigns it to the section
                                     int subSecInt = Convert.ToInt32(256 - Math.Pow(2, 8 - subSecBits));
                                     subnetInt[j] = subSecInt;
 
+                                    //Prefix to binary
+                                    for(int k = subSecBits; k > 0; k--)
+                                    {
+                                        subnetBin[j] += '1';
+                                        subnetBin[j].PadRight(8, '0');
+
+                                    }
+
                                     //formats the SubMask in a nice way with points for showing the user
                                     subMaskStr += Convert.ToString(subSecInt) + '.';
+                                    subMaskBin += subnetBin[j] + '.';
                                 }
+                                //Removes the last point on the string
                                 subMaskStr = subMaskStr.TrimEnd('.');
+                                subMaskBin = subMaskBin.TrimEnd('.');
                             }
                             //this means the subnet is a dotted decimal
                             else if (subMaskRaw.Length > 2 && subMaskRaw.Length <= 15)
@@ -108,6 +131,16 @@ namespace FinalProjectGF2
                                 subnetExploded = subMaskRaw.Split('.');
                                 subnetInt = testForErrors(subnetExploded);
 
+                                //Gets the binary value of the subnet mask
+                                subnetBin = intArrToBinArr(subnetInt);
+
+                                //Beautifies the subMask binary to show to the user
+                                foreach (string subMaskSec in subnetBin)
+                                {
+                                    subMaskBin += subMaskSec + '.';
+                                }
+                                subMaskBin = subMaskBin.TrimEnd('.');
+
                                 //The one that's gonna be shown to the user
                                 subMaskStr = subMaskRaw;
                             }
@@ -116,7 +149,18 @@ namespace FinalProjectGF2
                                 Start();
                             }
 
+                            //Gets an array of the ip sections in decimal and one in binary
                             ipExplodedInt = testForErrors(ipExploded);
+                            ipBin = intArrToBinArr(ipExplodedInt);
+                            Console.WriteLine(ipExplodedInt[1]);
+                            Console.ReadKey();
+
+                            //Beautifies the binary ip address that's going to be shown to the user
+                            foreach (string ipSec in ipBin)
+                            {
+                                ipAddrBin += ipSec + '.';
+                            }
+                            ipAddrBin = ipAddrBin.TrimEnd('.');
 
                             //Tests an Ip addr / sub mask for inconsistencies and returns an array with the values of each section
                             int[] testForErrors(string[] inputArr)
@@ -132,22 +176,34 @@ namespace FinalProjectGF2
                                 {
                                     //If any ip section is not an integer, it will return an error
                                     //It will also test for a number that is in the range 0-255
-                                    foreach (string ip in inputArr)
+                                    int j = 0;
+                                    foreach (string sec in inputArr)
                                     {
-                                        var j = 0;
-                                        if (!int.TryParse(ip, out int i) || i > 255 || i < 0)
+                                        int i;
+                                        if (!int.TryParse(sec, out i) || i > 255 || i < 0)
                                         {
                                             Start();
                                         }
                                         else
                                         {
                                             inputExplodedInt[j] = i;
-                                            j++;
                                         }
+                                        j++;
                                     }
                                 }
 
                                 return inputExplodedInt;
+                            }
+                            //Converts an array of integers into an array of binary strings
+                            string[] intArrToBinArr(int[] inputArr)
+                            {
+                                string[] outputBin = new string[4];
+                                foreach (int ipSec in inputArr)
+                                {
+                                    int i = 0;
+                                    outputBin[i] = DecToBinary(inputArr[0]);
+                                }
+                                return outputBin;
                             }
                         }
                     }
